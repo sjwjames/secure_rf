4f83d7b13da0bb9120892328a0b5387b0608672e
shenjianwei <shenjianwei@xindong.com>
2019-10-23 16:49:17 -0700

    add threadPool

--- src/utils/src/thread_pool.rs
+++ src/utils/src/thread_pool.rs
@@ -1,3 +1,99 @@
-pub mod thread_pool{
+pub mod thread_pool {
+    use std::sync::{mpsc, Mutex, Arc};
+    use std::thread;
 
+    pub enum Message {
+        NewJob(Job),
+        Terminate,
+    }
+
+    pub struct ThreadPool {
+        workers: Vec<Worker>,
+        sender: mpsc::Sender<Message>,
+    }
+
+    pub struct Worker {
+        id: usize,
+        thread: Option<thread::JoinHandle<()>>,
+    }
+
+    pub trait FnBox {
+        fn call_box(self: Box<Self>);
+    }
+
+    impl<F: FnOnce()> FnBox for F {
+        fn call_box(self: Box<Self>) {
+            (*self)()
+        }
+    }
+
+    pub type Job = Box<FnBox + Send + 'static>;
+
+
+    impl Worker {
+        pub fn new(id: usize, receiver: mpsc::Receiver<Arc<Mutex<mpsc::Receiver<Message>>>>) -> Worker {
+            let thread = thread::spawn(move || {
+                while let Ok(message) = receiver.lock().unwrap.recv() {
+                    match message {
+                        Message::NewJob(job) => {
+                            println!("Worker {} got a job;executing.", id);
+                            job.call_box();
+                        }
+                        Message::Terminate => {
+                            println!("Worker {} was told to terminate", id);
+                            break;
+                        }
+                    }
+                }
+            });
+            Worker {
+                id,
+                thread: Some(thread),
+            }
+        }
+    }
+
+    impl ThreadPool {
+        pub fn execute<F>(&self, f: F)
+            where
+                F: FnOnce() + Send + 'static
+        {
+            let job = Box::new(f);
+            self.sender.send(Message::NewJob(job)).unwrap();
+        }
+
+        pub fn new(size: usize) -> ThreadPool {
+            assert!(size > 0);
+            let mut workers = Vec::with_capacity(size);
+            let (sender, receiver) = mpsc::channel();
+            let receiver = Arc::new(Mutex::new(receiver));
+            for id in 0..size {
+                workers.push(Worker::new(id, Arc::clone(&receiver)));
+            }
+
+            ThreadPool {
+                workers,
+                sender,
+            }
+        }
+    }
+
+    impl Drop for ThreadPool {
+        fn drop(&mut self) {
+            println!("Sending terminate message to all workers.");
+
+            for _ in &mut self.workers {
+                self.sender.send(Message::Terminate).unwrap();
+            }
+
+            println!("Shutting down all workers");
+
+            for worker in &mut self.workers {
+                println!("Shutting down worker {}", worker.id);
+                if let Some(thread) = worker.thread.take() {
+                    thread.join().unwrap();
+                }
+            }
+        }
+    }
 }
