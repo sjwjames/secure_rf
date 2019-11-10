2b42d91d07e2d38ba44c4a596262027ccc853926
shenjianwei <shenjianwei@xindong.com>
2019-11-09 17:55:12 -0800

    fix equality_big_integer syntax

--- src/protocol.rs
+++ src/protocol.rs
@@ -88,32 +88,33 @@ pub mod protocol {
         result
     }
 
-//    pub fn equality_big_integer(x: &BigUint, y: &BigUint, ctx: &mut ComputingParty) -> BigUint {
-//        let equality_share = get_current_equality_share(ctx);
-//        let bigint_share = get_current_bigint_share(ctx);
-//        let prime = &ctx.dt_training.big_int_prime;
-//        let diff = big_uint_subtract(x, y, prime);
-//        let mut diff_list = Vec::new();
-//        diff_list.push(big_uint_subtract(&diff, &bigint_share.0, prime));
-//        diff_list.push(big_uint_subtract(equality_share, &bigint_share.1, prime));
-//        let mut in_stream = ctx.in_stream.try_clone()
-//            .expect("failed cloning tcp o_stream");
-//
-//        let mut o_stream = ctx.o_stream.try_clone()
-//            .expect("failed cloning tcp o_stream");
-//
-//        let mut message = serialize_biguint_vec(diff_list);
-//        o_stream.write((message + "\n").as_bytes());
-//
-//        let mut reader = BufReader::new(in_stream);
-//        let mut diff_list_message = String::new();
-//        reader.read_line(&mut diff_list_message).expect("fail to read diff list message");
-//
-//        let mut diff_list = deserialize_biguint_vec(diff_list_message);
-//
-//        let d = big_uint_subtract(&diff, &bigint_share.0, prime).add(&diff_list[0]).mod_floor(prime);
-//        let e = big_uint_subtract(&diff, &bigint_share.0, prime).add(&diff_list[0]).mod_floor(prime);
-//        let product = bigint_share.2.add(d.mul(&bigint_share.1)).add(&bigint_share.0.mul(e)).add(d.mul(&e).mul(BigUint::from(ctx.asymmetric_bit)));
-//        product
-//    }
+    pub fn equality_big_integer(x: &BigUint, y: &BigUint, ctx: &mut ComputingParty) -> BigUint {
+        let equality_share = get_current_equality_share(ctx);
+        let bigint_share = get_current_bigint_share(ctx);
+        let prime = &ctx.dt_training.big_int_prime;
+        let diff = big_uint_subtract(x, y, prime);
+        let mut diff_list = Vec::new();
+        diff_list.push(big_uint_subtract(&diff, &bigint_share.0, prime));
+        diff_list.push(big_uint_subtract(equality_share, &bigint_share.1, prime));
+        let mut in_stream = ctx.in_stream.try_clone()
+            .expect("failed cloning tcp o_stream");
+
+        let mut o_stream = ctx.o_stream.try_clone()
+            .expect("failed cloning tcp o_stream");
+
+        let mut message = serialize_biguint_vec(diff_list);
+        o_stream.write((message + "\n").as_bytes());
+
+        let mut reader = BufReader::new(in_stream);
+        let mut diff_list_message = String::new();
+        reader.read_line(&mut diff_list_message).expect("fail to read diff list message");
+
+        let mut diff_list = deserialize_biguint_vec(diff_list_message);
+
+        let d = big_uint_subtract(&diff, &bigint_share.0, prime).add(&diff_list[0]).mod_floor(prime);
+        let e = big_uint_subtract(&diff, &bigint_share.0, prime).add(&diff_list[0]).mod_floor(prime);
+        let product = big_uint_clone(&bigint_share.2).add(&big_uint_clone(&d).mul(&big_uint_clone(&bigint_share.1)))
+                  .add(big_uint_clone(&bigint_share.0).mul(big_uint_clone(&e))).add(big_uint_clone(&d).mul(&big_uint_clone(&e)).mul(BigUint::from(ctx.asymmetric_bit)));
+        product
+    }
 }
